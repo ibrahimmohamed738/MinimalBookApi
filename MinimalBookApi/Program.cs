b50e92bca46fd076c12b076dd5db5a3ea77679dc
ibrahim <ibrahim.mohamed738@gmail.com>
2024-07-04 17:05:34 +0300

    Used database instead of list data

--- MinimalBookApi/Program.cs
+++ MinimalBookApi/Program.cs
@@ -1,10 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalBookApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BookDb"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,60 +22,60 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var books = new List<Book>
-{
-    new() { Id=1, Title="The Hitchhiker's Guide to the Galaxy", Auther="Douglas Adams" },
-    new() { Id=2, Title="1984", Auther="George Orwell" },
-    new() { Id=3, Title="Ready Player One", Auther="Ernest Cline" },
-    new() { Id=4, Title="The Martian", Auther="Andy Weir" },
-};
+//var books = new List<Book>
+//{
+//    new() { Id=1, Title="The Hitchhiker's Guide to the Galaxy", Auther="Douglas Adams" },
+//    new() { Id=2, Title="1984", Auther="George Orwell" },
+//    new() { Id=3, Title="Ready Player One", Auther="Ernest Cline" },
+//    new() { Id=4, Title="The Martian", Auther="Andy Weir" },
+//};
 
-app.MapGet("/book", () =>
+app.MapGet("/book", async (DataContext context) =>
 {
-    return books;
+    return await context.Books.ToListAsync();
 });
 
-app.MapGet("/book/{id}", (int id) =>
+app.MapGet("/book/{id}", async (DataContext context, int id) =>
+  await context.Books.FindAsync(id) is Book book ?
+  Results.Ok(book)
+  : Results.NotFound("Sorry book not found."));
+
+
+app.MapPost("/book", async (DataContext context, Book book) =>
 {
-    var book = books.Find(b => b.Id == id);
-    if (book is null)
-        return Results.NotFound("Sorry, Book not found.");
-    return Results.Ok(book);
+    context.Books.Add(book);
+    await context.SaveChangesAsync();
+    return Results.Ok(await context.Books.ToListAsync());
 });
 
-app.MapPost("/book", (Book book) =>
+app.MapPut("/book/{id}", async (DataContext context, Book updatedBook, int id) =>
 {
-    books.Add(book);
-    return Results.Ok(books);
-});
-
-app.MapPut("/book/{id}", (Book updatedBook, int id) =>
-{
-    var book = books.Find(b => b.Id == id);
+    var book = await context.Books.FindAsync(id);
     if (book is null)
         return Results.NotFound("Sorry, Book not found.");
 
     book.Title = updatedBook.Title;
     book.Auther = updatedBook.Auther;
 
-    return Results.Ok(books);
+    await context.SaveChangesAsync();
+    return Results.Ok(await context.Books.ToListAsync());
 });
 
 
-app.MapDelete("/book/{id}", (int id) =>
+app.MapDelete("/book/{id}", async (DataContext context, int id) =>
 {
-    var book = books.Find(b => b.Id == id);
+    var book = await context.Books.FindAsync(id);
     if (book is null)
         return Results.NotFound("Sorry, Book not found.");
 
-    books.Remove(book);
-
-    return Results.Ok(books);
+    context.Books.Remove(book);
+    await context.SaveChangesAsync();
+    return Results.Ok(await context.Books.ToListAsync());
 });
 
 app.Run();
 
-class Book
+public class Book
 {
     public int Id { get; set; }
     public required string Title { get; set; }
